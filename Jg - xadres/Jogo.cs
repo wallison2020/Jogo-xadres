526a37cb05986a1fdebcbdfa7241e6bb4ed94175
Wallison Gregorio <wallisongregorio5@gmail.com>
2020-03-31 11:50:54 -0300

    Camada tabuleiro : Classe posição

--- Jg - xadres/Jogo.cs
+++ Jg - xadres/Jogo.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Tabuleiro;
 
 namespace Jg___xadres
 {
@@ -6,7 +7,8 @@ namespace Jg___xadres
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao p = new Posicao(3, 4);
+            Console.WriteLine("posição : "+ p);
         }
     }
 }
